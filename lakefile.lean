21ec89843380dfe903a194032fb53d8b456bcaf8
Spandan Ghosh <spandanghosh@iisc.ac.in>
2022-06-26 13:38:15 +0530

    Update

--- lakefile.lean
+++ lakefile.lean
@@ -1,15 +1,15 @@
 import Lake
 open Lake DSL
 
-package foo {
+package graphs_groups {
   -- add package configuration options here
 }
 
-lean_lib Foo {
+lean_lib GraphsGroups {
   -- add library configuration options here
 }
 
 @[defaultTarget]
-lean_exe foo {
+lean_exe graphs_groups {
   root := `Main
 }
